d22e39085c57e1a2d7435907169d1b5668e74010
Sergei Krainyukov <sk-new@mail.ru>
2022-03-24 15:14:24 +0300

    added login fragment and main fragment

---
+++ app/src/main/java/com/example/agoratestandroid/scenes/login/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.example.agoratestandroid.scenes.login
+
+import androidx.fragment.app.Fragment
+import com.example.agoratestandroid.R
+
+class LoginFragment : Fragment(R.layout.scene_login) {
+
+}
---
+++ app/src/main/java/com/example/agoratestandroid/scenes/main/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.example.agoratestandroid.scenes.main
+
+import androidx.fragment.app.Fragment
+import com.example.agoratestandroid.R
+
+class MainFragment : Fragment(R.layout.scene_main) {
+}
